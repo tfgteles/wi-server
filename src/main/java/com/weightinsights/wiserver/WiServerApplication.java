5cdcfbd39a78e6c021d6f7b8bbe2cf4587e36e72
Tiago Teles <tteles1@ford.com>
2022-11-19 12:28:17 -0500

    Create project

---
+++ src/main/java/com/weightinsights/wiserver/WiServerApplication.java
@@ -0,0 +1,13 @@
+package com.weightinsights.wiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weightinsights/wiserver/WiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weightinsights.wiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
